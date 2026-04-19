e606675cb3bf273627052843c1826cedd25d7987
Anand Raj Singh <singhanand31019@gmail.com>
2026-04-19 20:19:17 +0530

    Add the healthcheck controller

--- src/controllers/healthcheck.controllers.js
+++ src/controllers/healthcheck.controllers.js
@@ -1,7 +1,9 @@
 import { ApiResponse } from "../utils/api-response.js";
-const healthCheck = (req, res) => {
+import { asyncHandler } from "../utils/asyncHandler.js";
+/**
+  const healthCheck = (req, res) => {
   try {
-    console.log("health check controller hit");
+    // console.log("health check controller hit");
     return res
       .status(200)
       .json(new ApiResponse(200, { mesage: `Server is running` }));
@@ -9,5 +11,11 @@ const healthCheck = (req, res) => {
     console.log(error);
   }
 };
+**/
+// more modified version of the function
+
+const healthCheck = asyncHandler(async (req, res) => {
+  res.status(200).json(new ApiResponse(200, { message: "Server is running" }));
+});
 
 export default healthCheck;
