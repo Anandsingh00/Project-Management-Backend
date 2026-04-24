683002fc7f26b79aa8421fad357e1df3412e5047
Anand Raj Singh <singhanand31019@gmail.com>
2026-04-24 20:06:22 +0530

    Mounted the validation middleware on ''/register' route

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,8 +1,12 @@
 import { registerUser } from "../controllers/auth.controllers.js";
 import { Router } from "express";
+import { validate } from "../middlewares/validate.middleware.js";
+import { userRegistrationValidator } from "../validators/index.js";
 
 const router = Router();
 
-router.route("/register").post(registerUser);
+router
+  .route("/register")
+  .post(userRegistrationValidator(), validate, registerUser);
 
 export default router;
