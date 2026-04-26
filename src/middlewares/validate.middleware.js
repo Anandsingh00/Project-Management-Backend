f7d886933a28928dbbc57b7789712ddd85db463f
Anand Raj Singh <singhanand31019@gmail.com>
2026-04-26 09:03:04 +0530

    Add the auth middleware

--- src/middlewares/validate.middleware.js
+++ src/middlewares/validate.middleware.js
@@ -2,6 +2,7 @@ import { validationResult } from "express-validator";
 import { ApiError } from "../utils/api-errors.js";
 
 const validate = (req, res, next) => {
+  console.log("inside validation middleware");
   const errors = validationResult(req);
 
   if (errors.isEmpty()) {
@@ -15,6 +16,9 @@ const validate = (req, res, next) => {
       [err.path]: err.msg,
     }),
   );
+  extractedErrors.map((error) => {
+    console.log(error);
+  });
 
   throw new ApiError(422, "Recieved data is not valid", extractedErrors);
 };
