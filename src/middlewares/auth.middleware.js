dbc89b9226e8e0a1d6277189caeff6541c4c1a9a
Anand Raj Singh <singhanand31019@gmail.com>
2026-04-29 18:21:36 +0530

    Fixed the typo 'emailVerificationToken'

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -16,7 +16,7 @@ const verifyJWT = asyncHandler(async (req, res, next) => {
     const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
 
     const user = await User.findById(decodedToken._id).select(
-      "-password -refreshToken -emailVeirficationToken -emailVerificationExpiry",
+      "-password -refreshToken -emailVerificationToken -emailVerificationExpiry",
     );
 
     if (!user) {
