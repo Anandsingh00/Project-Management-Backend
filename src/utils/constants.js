00b737f8c418ec100ba58833fd40a3c4a31872c1
Anand Raj Singh <singhanand31019@gmail.com>
2026-05-06 07:25:57 +0530

    .

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -5,7 +5,8 @@ export const UserRolesEnum = {
 };
 
 export const AvailableUserRole = Object.values(UserRolesEnum);
-// Object.values: ["value1","value2","value3"] (array of values of the object which is passed to this function)
+// Object.values: ["value1","value2","value3"]
+//  (array of values of the object which is passed to this function)
 
 export const TaskStatusEnum = {
   TODO: "todo",
